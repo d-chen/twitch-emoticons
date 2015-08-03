9a5b24aa6660d8a6d68e5d11f4cdb553a79797f9
Danny Chen <dannychen210@gmail.com>
2015-08-02 23:04:50 -0700

    CoolCat DendiFace KappaPride etc

--- download.py
+++ download.py
@@ -60,6 +60,52 @@ def get_emote_list():
         result = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(resp.text)
         return result['channels']['srkevo1']['emotes']
 
-emote_list = get_emote_list()
+#emote_list = get_emote_list()
 #download_emotes(emote_list, "srkevo1")
-create_json(emote_list, "srkevo1")
+#create_json(emote_list, "srkevo1")
+
+
+def create_global_json(emote_list):
+    my_list = []
+    TEMPLATE = "https://raw.githubusercontent.com/d-chen/twitch-emoticons/master/global/{0}.png"
+    REJECT = ['CougarHunt', 'EagleEye', 'RedCoat', 'StoneLightning', 'TheRinger']
+
+    for key, value in emote_list.iteritems():
+        my_dict = {"id": key, "src": TEMPLATE.format(key)}
+        if not key in REJECT:
+            my_list.append(my_dict)
+
+    with open('global.json', 'w') as file:
+        json.dump(my_list, file)
+
+def download_global_emotes(emote_list):
+    TEMPLATE = "http://static-cdn.jtvnw.net/emoticons/v1/{image_id}/1.0"
+
+    for key, value in emote_list.iteritems():
+        url = TEMPLATE.format(image_id=value['image_id'])
+        path = './global/{id}.png'.format(id=key)
+
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            logger.info('Saving image to {path}'.format(path=path))
+            with open(path, 'wb') as file:
+                for chunk in r.iter_content():
+                    file.write(chunk)
+    logger.info('Finished downloading emotes')
+
+def get_global_emote_list():
+    EMOTE_LIST_URL = "http://twitchemotes.com/api_cache/v2/global.json"
+
+    logger.info('Requesting emote list from {0}'.format(EMOTE_LIST_URL))
+    resp = requests.get(EMOTE_LIST_URL)
+
+    if (resp.status_code != 200):
+        logger.error('Cannot get emote list. Status code={0}'.format(resp.status_code))
+    else:
+        result = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(resp.text)
+        return result['emotes']
+    
+    
+emotes = get_global_emote_list()
+download_global_emotes(emotes)
+create_global_json(emotes)
